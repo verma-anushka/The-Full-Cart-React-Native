91715b5b0614e8a81720cb5d9667bec7b8532b80
Anushka Verma <v.anushka786@gmail.com>
2021-03-15 02:12:03 +0530

    setup: custom fonts

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import store from "./src/store";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+import Navigator from "./src/navigation";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    openSans: require("./assets/fonts/OpenSans-Regular.ttf"),
+    openSansBold: require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
